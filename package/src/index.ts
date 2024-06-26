321510e33847a89e3f0645fe3acf3d86bb96f416
gilzonme <gilzon.me@gmail.com>
2024-06-26 10:28:45 +0530

    Updates

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,5 +1,4 @@
 import YoutubePreview from './components/YoutubePreview';
 import UPI from './components/UPI';
-import WebStories from './components/Story';
 
-export { YoutubePreview,UPI,WebStories };
+export { YoutubePreview,UPI };
